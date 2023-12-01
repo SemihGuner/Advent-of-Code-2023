1f1be20d04533552421b05fb95890b9e45cb6876
Semih Güner <semih200279@ogr.duzce.edu.tr>
2023-12-01 18:45:09 +0300

    code optimization

--- day_one.py
+++ day_one.py
@@ -4,6 +4,7 @@ entry = aocd.get_data(day=1,year=2023)
 spData = entry.split('\n')
 
 toplam = 0
+digitNums = [3,4,5]
 numbers = ["one","two","three","four","five","six","seven","eight","nine"] 
 for i in range(len(spData)):
     #print("start of i")
@@ -16,49 +17,39 @@ for i in range(len(spData)):
             print("i-j: {}-{}".format(i,j)) 
         else:
             # Part 2  
-            araniyor=""
-            check = True
-            for p in range(len(numbers)): 
+            araniyor="" 
+            for p in range(len(digitNums)): 
                 #print("start of p")
-                numLen = len(numbers[p]) 
+                numLen = digitNums[p]
                 araniyor = spData[i][j:j+numLen]
                 print("i-j-p: {}-{}-{} ; araniyor: {} ; numLen: {}".format(i,j,p,araniyor,numLen)) 
-                if (araniyor == numbers[0]) & check:
+                if (araniyor == numbers[0]):
                     yeniStr+="1"
                     print("Gotcha! YeniStr= " , yeniStr)  
-                    check=False
-                elif (araniyor == numbers[1]) & check:
+                elif (araniyor == numbers[1]):
                     yeniStr+="2"
                     print("Gotcha! YeniStr= " , yeniStr)   
-                    check=False
-                elif (araniyor == numbers[2]) & check:
+                elif (araniyor == numbers[2]):
                     yeniStr+="3"
                     print("Gotcha! YeniStr= " , yeniStr)   
-                    check=False
-                elif (araniyor == numbers[3]) & check:
+                elif (araniyor == numbers[3]):
                     yeniStr+="4"
                     print("Gotcha! YeniStr= " , yeniStr)   
-                    check=False
-                elif (araniyor == numbers[4]) & check:
+                elif (araniyor == numbers[4]):
                     yeniStr+="5"
                     print("Gotcha! YeniStr= " , yeniStr)   
-                    check=False
-                elif (araniyor == numbers[5]) & check:
+                elif (araniyor == numbers[5]):
                     yeniStr+="6"
                     print("Gotcha! YeniStr= " , yeniStr)   
-                    check=False
-                elif (araniyor == numbers[6]) & check:
+                elif (araniyor == numbers[6]):
                     yeniStr+="7"
                     print("Gotcha! YeniStr= " , yeniStr)   
-                    check=False
-                elif (araniyor == numbers[7]) & check:
+                elif (araniyor == numbers[7]):
                     yeniStr+="8"
                     print("Gotcha! YeniStr= " , yeniStr)   
-                    check=False
-                elif (araniyor == numbers[8]) & check:
+                elif (araniyor == numbers[8]):
                     yeniStr+="9"
                     print("Gotcha! YeniStr= " , yeniStr)   
-                    check=False
     donusecekStr = yeniStr[1] + yeniStr[-1]
     toplam+=int(donusecekStr) 
 print("Summation:" , toplam)
